39c1f7733b6073315fb954adf46bcb8698de675c
Ruvilo <rubenvillaltalopez12@gmail.com>
2023-02-11 23:24:20 -0600

    files commented

--- technicalTest1.py
+++ technicalTest1.py
@@ -1,3 +1,10 @@
+"""
+This recursive function counts all the digits of closed paths that could contain 
+a number.
+
+    I: num::int
+    O: closedPathCount::int
+"""
 def closedPath(num):
     
     if (num == 0):
@@ -5,9 +12,15 @@ def closedPath(num):
     else:
         return closedPath_aux(num,0)
 
+"""
+This is an helper recursive function, it helps to add a new parameter to 
+manage the closed paths counter.
+    I: num::int, closedPathCount
+    O: closedPathCount:: int
+"""
 def closedPath_aux(num,closedPathCount):
                     #(0,1,2,3,4,5,6,7,8,9)
-    closedPathValues=(1,0,0,0,1,0,1,1,2,1)
+    closedPathValues=(1,0,0,0,1,0,1,0,2,1)
     
     if(num==0):
         return closedPathCount
@@ -15,5 +28,8 @@ def closedPath_aux(num,closedPathCount):
         return closedPath_aux(num//10, closedPathCount+closedPathValues[num%10])
 
 
+""" 
+Those lines displays the result of closedPath function.
+"""
 print(closedPath(630)) #---> 2
 print(closedPath(1288)) #---> 4
